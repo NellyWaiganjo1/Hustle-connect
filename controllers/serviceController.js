6ea10be4eebb497574b30b71b5a8771e1557b7fb
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-08-23 01:06:45 +0300

    Add getServices controller for fetching all services.

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -23,3 +23,24 @@ const createService = async(req, res) => {
     }
 };
 
+const getServices = async (req,res) => {
+    try{
+        //Handle search and filter using query parameters
+        const keyword = req.query.keyword 
+        ? {
+            serviceName: {
+                $regex: req.query.keyword,
+                $options: 'i' // case-insensitive search
+            },
+        }
+        : {};
+
+        const category = req.query.category ? { category: req.query.category } : {};
+
+        // Find services in the database matching the search criteria
+        const services = await Service.find({...keyword, ...category }).populate('provider', 'name');
+        res.json(services);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
