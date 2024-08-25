1463414b308c6d55e7e061063f64201a8d3da7b7
VICKIE MWENDWA <mwendwavickie@gmail.com>
2024-08-25 19:38:58 +0300

    Add routes for creating, reading, updating, and deleting user profiles.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,3 +9,16 @@ const {
     deleteUserProfile
 } = require('../controllers/userController');
 
+// POST route to create a new user profile
+router.post('/', createUserProfile);
+
+// GET route to retrieve a user profile by ID
+router.get('/:id', getUserProfileById);
+
+// PUT route to update a user profile by ID
+router.put('/:id', updateUserProfile);
+
+// DELETE route to remove a user profile by ID
+router.delete('/:id', deleteUserProfile);
+
+module.exports = router;
