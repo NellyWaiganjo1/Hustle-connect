afca7564dba6ae192425c3e481e4edd8da2a7dc5
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-08-25 20:21:46 +0300

    created the auth middleware

---
+++ utils/authMiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    // Get the token from the header
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+
+    try {
+        // Verify the token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Add user info to request object
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' });
+    }
+};
+
+module.exports = auth;
