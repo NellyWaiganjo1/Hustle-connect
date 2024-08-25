eccd5a0e57a3073fe19eac03491ead981e12fed2
Nelly Waiganjo <nellywaiganjo9@gmail.com>
2024-08-25 20:20:21 +0300

    apdated app.js

--- app.js
+++ app.js
@@ -1,55 +1,23 @@
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const createError = require('http-errors');
+const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
 
-// Connect to the database
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1);
-    }
-};
-
+// Connect to database
 connectDB();
 
 // Middleware
-app.use(logger('dev'));
-app.use(express.json()); // Parse JSON bodies
-app.use(express.urlencoded({ extended: false })); // Parse URL-encoded bodies
-app.use(cookieParser()); // Parse cookies
-
-// Static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // To parse JSON bodies
 
 // Routes
 app.use('/api/users', userRoutes);
 
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// Error handler
+// Error handling middleware
 app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // Send error response
-  res.status(err.status || 500);
-  res.json({ message: err.message, error: err });
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 module.exports = app;
